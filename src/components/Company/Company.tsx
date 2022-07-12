52ce8f196ba18754ae93b074ef7e6917f820ccf1
Роман Марич <you@example.com>
2022-07-12 12:57:08 +0300

    Adede new file

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@material-ui/core'
 import React, { ChangeEvent, FC, ReactNode, useEffect, useState } from 'react'
-
 import { getCompaniesSelector } from '../../redux/company-selector'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -8,10 +7,9 @@ import { useParams } from 'react-router-dom'
 export const Company: FC = () => {
   const companies = useSelector(getCompaniesSelector)
   let params = useParams()
-  let company = companies.find(c => c.name.replace(/\s+/g, '-').toLowerCase()  ===  params.id ) 
+  let company = companies.find(c => c.name.replace(/\s+/g, '-').toLowerCase() === params.id)
 
-  console.log(company)
-  const [string, setString] = useState<string | null | undefined>(company && company.boxes )
+  const [string, setString] = useState<string | null | undefined>(company && company.boxes)
   const [cargoBays, setCargoBays] = useState<ReactNode>(0)
 
   const numberOfRequired = (boxes: string | null | undefined) => {
@@ -31,23 +29,23 @@ export const Company: FC = () => {
     setString(e.target.value)
   }
 
-  return  <Box style={{ margin: 15 }}>
-   {company && <div> 
-    <h1>{company.name}</h1>
-    <a href={`mailto:${company.email}`} target='_blanc' >{company.email}</a>
-    <h3 >Number of required cargo bays {cargoBays}</h3>
-    <h3>Cargo boxes</h3>
-    <input
-      value={string as string}
-      onChange={onChange}
-      type="text"
-      style={{
-        fontSize: 16,
-        border: '3px solid #000',
-        width: 300,
-        padding: 5
-      }}
-    />
+  return <Box style={{ margin: 15 }}>
+    {company && <div>
+      <h1>{company.name}</h1>
+      <a href={`mailto:${company.email}`} target='_blanc' >{company.email}</a>
+      <h3 >Number of required cargo bays {cargoBays}</h3>
+      <h3>Cargo boxes</h3>
+      <input
+        value={string as string}
+        onChange={onChange}
+        type="text"
+        style={{
+          fontSize: 16,
+          border: '3px solid #000',
+          width: 300,
+          padding: 5
+        }}
+      />
     </div>}
   </Box>
 }
